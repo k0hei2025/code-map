8cd168de66668d34f088dd021c2e187802afa1c1
Rutwik1801 <65919321+Rutwik1801@users.noreply.github.com>
2021-06-29 21:28:23 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../components/Editor.css'
 import '../components/prism.css'
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// import {Provider} from 'react-redux'
+// import  store from '../store/store'
+// import withRedux from 'next-redux-wrapper'
+
+// import { Store } from '@material-ui/icons'
+// function MyApp({ Component, pageProps }) {
+//   return <Provider  store={store}> <Component {...pageProps} /> </Provider>
+// }
+
+// export default withRedux(store) (MyApp) 
+
+import { Provider } from 'react-redux'
+import { store } from '../store/store'
+// import '../styles/globals.css'
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
+
