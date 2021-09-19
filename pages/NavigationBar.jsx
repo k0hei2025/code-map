a3ce339413ee429be68358a334d4818337afb872
k0hei2025 <vaibhavvermaonline@gmail.com>
2021-09-19 13:53:37 +0530

    add empty master

--- pages/NavigationBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Navigation from '../components/NavigationBar/upperNavigationBar'
-
-
-export default function NavigationBar() {
-               return ( 
-                    <Navigation/>          
-               )
-}
